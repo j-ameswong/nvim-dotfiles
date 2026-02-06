b1757ee0463509ea2142bfa085d27642f0dd8a31
Cheng An Wong <wongchengan@gmail.com>
2026-02-06 21:51:28 +0000

    feat: fixed severty sorting of diagnostics

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
 vim.opt.title = true
 vim.g.python3_host_prog = '~/.venv/bin/python3'
 
+vim.diagnostic.config({
+  severity_sort = true,
+  underline = true,
+  
+  virtual_text = {
+    severity = { min = vim.diagnostic.severity.WARN }
+  },
+})
+
 require("config.keymaps")
 require("config.options")
 require("config.lazy")
