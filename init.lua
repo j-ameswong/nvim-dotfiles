7a6976110dfd4de0f374f180c8aaacba08174ab1
Cheng An Wong <wongchengan@gmail.com>
2026-08-29 21:00:43 +0100

    feat: support for kotlin and llama

--- init.lua
+++ init.lua
@@ -21,8 +21,17 @@ vim.lsp.config('solargraph', {
     }
   }
 })
-
 vim.lsp.enable('solargraph')
+
+vim.g.llama_config = {
+  endpoint_fim = "http://127.0.0.1:8012/infill",
+  n_prefix = 256,
+  n_suffix = 64,
+  n_predict = 128,
+  t_max_prompt_ms = 500,
+  t_max_predict_ms = 1000,
+  show_info = 0,
+}
 
 require("config.keymaps")
 require("config.options")
